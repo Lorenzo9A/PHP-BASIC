9e6ef960a997cd49f6b7fd2b35d4b7c45eb89da4
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-13 14:12:58 +0200

    variabelen taak 1 klaar

---
+++ 2-Variabelen-en-datatype/taak01/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+    
+    
+    
+    <?php 
+  
+
+  $score = 0;
+  
+  echo "$score";
+  
+  ?>
+  
+
+
+
+     
+  </body>
+
+    
+
+</html>
