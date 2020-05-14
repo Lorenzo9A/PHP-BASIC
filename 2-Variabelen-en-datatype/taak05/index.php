0ceb6e820f12405dabb805ce678901923b67d4ed
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-14 12:02:24 +0200

    taak 5 af

---
+++ 2-Variabelen-en-datatype/taak05/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $voornaam = "Lorenzo";
+  
+  echo gettype($voornaam);
+  
+  
+  
+  ?>
+    
+  </body>
+
+    
+</html>
