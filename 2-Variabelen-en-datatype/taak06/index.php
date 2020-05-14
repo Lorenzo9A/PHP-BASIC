dee02fe6621e9a26186b98709203a4521c64bc0a
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-14 12:38:02 +0200

    taak 6 af

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $voornaam = "Lorenzo";
+  $leeftijd = 17;
+  $getal = 10.5;
+  $ja = true;
+  $geen = null;
+  
+  echo gettype($voornaam);
+  echo gettype($leeftijd);
+  echo gettype($getal);
+  echo gettype($ja);
+  echo gettype($geen);
+  
+  
+  ?>
+    
+  </body>
+
+    
+</html>
