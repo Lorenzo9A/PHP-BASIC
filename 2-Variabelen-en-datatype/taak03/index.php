e57bd1bccae8a9a1253e1c64d9eec39fd4f2c625
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-14 11:34:02 +0200

    taak 3 af

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $leeftijd = 17;
+  $melk = 1.65;
+  
+  echo "$leeftijd <br>";
+  echo "$melk";
+  
+  ?>
+  
+     
+  </body>
+
+    
+
+</html>
