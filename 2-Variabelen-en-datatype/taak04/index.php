0a4bd96e248c9b36f4bfaedcb3648d131af19a01
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-14 11:41:48 +0200

    taak 4 af

---
+++ 2-Variabelen-en-datatype/taak04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $test = "true";
+  
+  echo "$test";
+  $test = null;
+  echo" $test";
+  
+  
+  ?>
+    
+  </body>
+
+    
+</html>
