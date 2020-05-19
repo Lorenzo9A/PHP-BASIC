cb0a661f4a4aed4b197f3806d1bd9f5eb2ad38d5
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-19 12:49:44 +0200

    taak 1 af

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $getal1 = 10;
+  $getal2 = 7;
+
+  echo $getal1 + $getal2;
+  echo "<br>";
+  echo $getal1 - $getal2;
+  echo "<br>";
+  echo $getal1 * $getal2;
+  echo "<br>";
+  echo $getal1 / $getal2;
+  
+  
+  ?>
+    
+  </body>
+
+    
+</html>
