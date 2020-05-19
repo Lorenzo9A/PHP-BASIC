b2d2c7286a9580209e5725eb364b82dabb391a3a
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-19 13:31:34 +0200

    taak 3 af

---
+++ 4-Arithmetics/taak03/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $getal1 = 10;
+  
+  echo ++$getal1;
+  echo "<br>";
+  echo --$getal1;
+  
+  ?>
+    
+  </body>
+
+    
+</html>
