44989e19202fed93a56386e2775985070eb1f411
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-19 12:41:02 +0200

    taak 2 af array

---
+++ 3-Arrays/taak02/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+ $myDogNames = ["Noah","Zoey","Tom"];
+
+ echo "$myDogNames[0]$myDogNames[1]$myDogNames[2]";
+
+ $myDogNames[2] = "Roos";
+ echo "<br>";
+ echo "$myDogNames[0]$myDogNames[1]$myDogNames[2]";
+
+
+  
+
+  
+  
+  
+  
+  ?>
+    
+  </body>
+
+    
+</html>
