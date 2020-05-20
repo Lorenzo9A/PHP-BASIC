0387eec9b9e5e84b87c4a9cec0e82091556f24e0
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 14:22:13 +0200

    af

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  function zoekLetterInAlfabet(){
+    $woordje = "hallo";
+    $zinnetje = "mooie blauwe planeet";
+    $heleZin = "Hallo, mooie blauwe planeet.";
+    echo $heleZin;
+}
+
+zoekLetterInAlfabet();
+
+  
+  ?>
+    
+  </body>
+
+    
+</html>
