d44b129e203d16ef72a453aa25f12318f95954ba
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 14:26:49 +0200

    af

---
+++ 7-Include-Require/taak01/menu.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+ $var1 = "schoen";
+ $var2 = "jas";
+ $var3 = "pet";
+
+ echo "<ul>$var1</ul><ul>$var2</ul><ul>$var3</ul>";
+
+
+  
+  ?>
+    
+  </body>
+
+    
+</html>
