a9912838d93a33f406d222abe28a9007539c01b7
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 13:41:35 +0200

    taak 5 af

---
+++ 5-Conditional_Statements/taak05/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  $score = 12;
+
+  switch($score) {
+      case 12:
+      echo "Je hebt alle punten!!";
+      break;
+      case 11:
+      echo "Nog 1tje";
+      break;
+      case 10:
+      echo "Wow wat een fantastische score!";
+      break;
+      case 9:
+      echo "Wow je bent in de buurt";
+      break;
+      case 8:
+      echo "Goed bezig, bijna alle punten!";
+      break;
+      case 7:
+      echo "Ga zo door!";
+      break;
+      default:
+      echo "Nog even oefenen dan haal je vast alle punten!";
+  }
+  
+  ?>
+    
+  </body>
+
+    
+</html>
