9485144079b5db2f3298e2a87b6355e48a720476
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 13:29:18 +0200

    taak 4 af

---
+++ 5-Conditional_Statements/taak04/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  $prod_besteld = 15;
+
+  if($prod_besteld < 10) {
+      $prijs = 1.50;
+  } elseif($prod_besteld < 20) {
+      $prijs = 1.25;
+  } else {
+      $prijs = 1.00;
+  }
+  echo $prijs;
+  
+  ?>
+    
+  </body>
+
+    
+</html>
