fa01ce7e7d238c2763dceedcde1df171aed017c3
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 12:02:21 +0200

    af

---
+++ 5-Conditional_Statements/taak01/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  
+
+  $test1 = 12;
+  $test2 = "Gewoon tekst";
+
+  var_dump($test1 == $test2);
+  echo"<br>";
+  var_dump($test1 === $test2);
+  echo"<br>";
+  var_dump($test1 != $test2);
+  echo"<br>";
+  var_dump($test1 > $test2);
+  echo"<br>";
+  var_dump($test1 < $test2);
+  echo"<br>";
+  var_dump($test1 <> $test2);
+  echo"<br>";
+  var_dump($test1 >= $test2);
+  echo"<br>";
+  var_dump($test1 <= $test2);
+  
+  ?>
+    
+  </body>
+
+    
+</html>
