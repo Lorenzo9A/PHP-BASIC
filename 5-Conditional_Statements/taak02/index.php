83efb19b650b543d04c7a1049e8d4d9d0c6ea185
Lorenzo Breedveld <2105144@talnet.nl>
2020-05-20 13:00:58 +0200

    taak 2 af

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Document</title>
+</head>
+<body>
+     
+    
+    <?php 
+  $test1 = 10;
+  $test2 = 6;
+
+  if($test1 == $test2){
+    echo "Gelijk";
+  }
+  echo"<br>";
+  if($test1 != $test2){
+    echo "ongelijk";
+  }
+  echo"<br>";
+  if($test1 === $test2){
+    echo "identiek";
+  }
+  echo"<br>";
+  if($test1 > $test2){
+    echo "test 1 is groter dan test 2";
+  }
+  echo"<br>";
+  if($test1 >= $test2){
+    echo "test 1 is groter of gelijk aan test 2";
+  }
+  echo"<br>";
+  if($test1 < $test2){
+    echo "test 1 is kleiner dan test 2";
+  }
+
+  
+  ?>
+    
+  </body>
+
+    
+</html>
